f3367ba96d7b273b5984f8549cb3303a27c63d84
Arif Dikkanat <dikkanata@mef.edu.tr>
2023-06-15 03:29:21 +0300

    Footer and Profile Page Update

--- src/views/PageDetails/ProfilePage.js
+++ src/views/PageDetails/ProfilePage.js
@@ -1,8 +1,11 @@
-import React, { useState } from 'react'
-import classnames from 'classnames'
+import React, { useState } from "react";
+import classnames from "classnames";
 // javascript plugin used to create scrollbars on windows
-import PerfectScrollbar from 'perfect-scrollbar'
+import PerfectScrollbar from "perfect-scrollbar";
 // reactstrap components
+import julie from "../../assets/img/julie.jpeg";
+import james from "../../assets/img/james.jpg";
+
 import {
   Button,
   Card,
@@ -24,89 +27,166 @@ import {
   Col,
   UncontrolledTooltip,
   UncontrolledCarousel,
-} from 'reactstrap'
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+} from "reactstrap";
 
 // core components
-import Footer from 'components/Footer/Footer.js'
-import IndexNavbar from 'components/Navbars/IndexNavbar'
+import Footer from "components/Footer/Footer.js";
+import IndexNavbar from "components/Navbars/IndexNavbar";
 
 const carouselItems = [
   {
-    src: require('assets/img/denys.jpg'),
-    altText: 'Slide 1',
-    caption: 'Big City Life, United States',
+    src: require("assets/img/denys.jpg"),
+    altText: "Slide 1",
+    caption: "Big City Life, United States",
   },
   {
-    src: require('assets/img/fabien-bazanegue.jpg'),
-    altText: 'Slide 2',
-    caption: 'Somewhere Beyond, United States',
+    src: require("assets/img/fabien-bazanegue.jpg"),
+    altText: "Slide 2",
+    caption: "Somewhere Beyond, United States",
   },
   {
-    src: require('assets/img/mark-finn.jpg'),
-    altText: 'Slide 3',
-    caption: 'Stocks, United States',
+    src: require("assets/img/mark-finn.jpg"),
+    altText: "Slide 3",
+    caption: "Stocks, United States",
   },
-]
+];
 
-let ps = null
+let ps = null;
 
 export default function ProfilePage() {
-  const [tabs, setTabs] = React.useState(1)
+  const [tabs, setTabs] = React.useState(1);
+  const [images, setImages] = React.useState([
+    {
+      image: julie,
+      propmt: "Gotum gibi kız yap",
+      negativePrompt: "gotum gibi",
+    },
+    {
+      image: james,
+      propmt: "Gotum gibi kız yap",
+      negativePrompt: "gotum gibi",
+    },
+  ]);
+
+  const [modal, setModal] = React.useState({ bool: false, image: null });
+
   React.useEffect(() => {
-    if (navigator.platform.indexOf('Win') > -1) {
-      document.documentElement.className += ' perfect-scrollbar-on'
-      document.documentElement.classList.remove('perfect-scrollbar-off')
-      let tables = document.querySelectorAll('.table-responsive')
+    if (navigator.platform.indexOf("Win") > -1) {
+      document.documentElement.className += " perfect-scrollbar-on";
+      document.documentElement.classList.remove("perfect-scrollbar-off");
+      let tables = document.querySelectorAll(".table-responsive");
       for (let i = 0; i < tables.length; i++) {
-        ps = new PerfectScrollbar(tables[i])
+        ps = new PerfectScrollbar(tables[i]);
       }
     }
-    document.body.classList.toggle('profile-page')
+    document.body.classList.toggle("profile-page");
     // Specify how to clean up after this effect:
     return function cleanup() {
-      if (navigator.platform.indexOf('Win') > -1) {
-        ps.destroy()
-        document.documentElement.className += ' perfect-scrollbar-off'
-        document.documentElement.classList.remove('perfect-scrollbar-on')
+      if (navigator.platform.indexOf("Win") > -1) {
+        ps.destroy();
+        document.documentElement.className += " perfect-scrollbar-off";
+        document.documentElement.classList.remove("perfect-scrollbar-on");
       }
-      document.body.classList.toggle('profile-page')
-    }
-  }, [])
+      document.body.classList.toggle("profile-page");
+    };
+  }, []);
 
-  const [user, setUser] = useState()
+  const [user, setUser] = useState();
 
   React.useEffect(() => {
-    const currentUser = window.localStorage.getItem('user')
-    setUser(JSON.parse(currentUser))
-  }, [])
+    const currentUser = window.localStorage.getItem("user");
+    setUser(JSON.parse(currentUser));
+  }, []);
+
+  const ImageCard = ({ image }) => {
+    return (
+      <div className="card">
+        <img src={image} height={"100%"} width={"100%"}></img>
+
+        <Button className="btn-neutral" color="primary">
+          >Download
+        </Button>
+        <Button color="danger">Delete</Button>
+      </div>
+    );
+  };
+
   return (
     <>
-      <IndexNavbar />
-      <div className='wrapper'>
-        <div className='page-header'>
+      <IndexNavbar />{" "}
+      <Modal
+        isOpen={modal.bool}
+        toggle={() => {
+          setModal({ image: null, bool: false });
+        }}
+      >
+        <div className="modal-header">
+          <h5 className="modal-title" id="exampleModalLabel">
+            TextaulVision
+          </h5>
+          <button
+            type="button"
+            className="close"
+            data-dismiss="modal"
+            aria-hidden="true"
+            onClick={() => {
+              setModal({ image: null, bool: false });
+            }}
+          >
+            <i className="tim-icons icon-simple-remove" />
+          </button>
+        </div>
+        <ModalBody>
+          <img src={modal.image} height={"100%"} width={"100%"}></img>{" "}
+        </ModalBody>
+        <ModalFooter>
+          <Button
+            color="primary"
+            onClick={() => {
+              setModal({ image: null, bool: false });
+            }}
+          >
+            Download
+          </Button>
+          <Button
+            color="secondary"
+            onClick={() => {
+              setModal({ image: null, bool: false });
+            }}
+          >
+            Delete
+          </Button>
+        </ModalFooter>
+      </Modal>
+      <div className="wrapper">
+        <div className="page-header">
           <img
-            alt='...'
-            className='dots'
-            src={require('assets/img/dots.png')}
+            alt="..."
+            className="dots"
+            src={require("assets/img/dots.png")}
           />
           <img
-            alt='...'
-            className='path'
-            src={require('assets/img/path4.png')}
+            alt="..."
+            className="path"
+            src={require("assets/img/path4.png")}
           />
-          <Container className='align-items-center'>
+          <Container className="align-items-center">
             <Row>
-              <Col lg='6' md='6'>
-                <h1 className='profile-title text-left'>{user?.displayName}</h1>
-                <h5 className='text-on-back'>01</h5>
-                <p className='profile-description'>
+              <Col lg="6" md="6">
+                <h1 className="profile-title text-left">{user?.displayName}</h1>
+                <h5 className="text-on-back">01</h5>
+                <p className="profile-description">
                   This is your profile page. You can see your entire journey on
                   TextualVision from here! Scroll down to see the masterpieces
                   you have created!
                 </p>
               </Col>
-              <Col className='ml-auto mr-auto' lg='4' md='6'>
-                <Card className='card-coin card-plain'>
+              <Col className="ml-auto mr-auto" lg="4" md="6">
+                {/*  <Card className='card-coin card-plain'>
                   <CardHeader>
                     {user?.photoURL && (
                       <img
@@ -250,151 +330,56 @@ export default function ProfilePage() {
                       </TabPane>
                     </TabContent>
                   </CardBody>
-                </Card>
+                </Card> */}
               </Col>
             </Row>
           </Container>
         </div>
-        <div className='section'>
+
+        <div className="section">
           <Container>
-            <Row className='justify-content-between'>
-              <Col md='6'>
-                <Row className='justify-content-between align-items-center'>
-                  <UncontrolledCarousel items={carouselItems} />
-                </Row>
-              </Col>
-              <Col md='5'>
-                <h1 className='profile-title text-left'>Projects</h1>
-                <h5 className='text-on-back'>02</h5>
-                <p className='profile-description text-left'>
-                  An artist of considerable range, Ryan — the name taken by
-                  Melbourne-raised, Brooklyn-based Nick Murphy — writes,
-                  performs and records all of his own music, giving it a warm,
-                  intimate feel with a solid groove structure. An artist of
-                  considerable range.
-                </p>
-                <div className='btn-wrapper pt-3'>
-                  <Button
-                    className='btn-simple'
-                    color='primary'
-                    href='#pablo'
-                    onClick={(e) => e.preventDefault()}
-                  >
-                    <i className='tim-icons icon-book-bookmark' /> Bookmark
-                  </Button>
-                  <Button
-                    className='btn-simple'
-                    color='info'
-                    href='#pablo'
-                    onClick={(e) => e.preventDefault()}
-                  >
-                    <i className='tim-icons icon-bulb-63' /> Check it!
-                  </Button>
-                </div>
-              </Col>
-            </Row>
-          </Container>
-        </div>
-        <section className='section'>
-          <Container>
-            <Row>
-              <Col md='6'>
-                <Card className='card-plain'>
-                  <CardHeader>
-                    <h1 className='profile-title text-left'>Contact</h1>
-                    <h5 className='text-on-back'>03</h5>
-                  </CardHeader>
-                  <CardBody>
-                    <Form>
-                      <Row>
-                        <Col md='6'>
-                          <FormGroup>
-                            <label>Your Name</label>
-                            <Input defaultValue='Mike' type='text' />
-                          </FormGroup>
-                        </Col>
-                        <Col md='6'>
-                          <FormGroup>
-                            <label>Email address</label>
-                            <Input placeholder='mike@email.com' type='email' />
-                          </FormGroup>
-                        </Col>
-                      </Row>
-                      <Row>
-                        <Col md='6'>
-                          <FormGroup>
-                            <label>Phone</label>
-                            <Input defaultValue='001-12321345' type='text' />
-                          </FormGroup>
-                        </Col>
-                        <Col md='6'>
-                          <FormGroup>
-                            <label>Company</label>
-                            <Input defaultValue='CreativeTim' type='text' />
-                          </FormGroup>
-                        </Col>
-                      </Row>
-                      <Row>
-                        <Col md='12'>
-                          <FormGroup>
-                            <label>Message</label>
-                            <Input placeholder='Hello there!' type='text' />
-                          </FormGroup>
-                        </Col>
-                      </Row>
-                      <Button
-                        className='btn-round float-right'
-                        color='primary'
-                        data-placement='right'
-                        id='tooltip341148792'
-                        type='button'
-                      >
-                        Send text
+            <Table>
+              <thead>
+                <tr>
+                  <th className="text-center">#</th>
+                  <th>Image</th>
+                  <th>Prompt</th>
+                  <th>Negative Prompt</th>
+                  <th className="text-right">Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                {images.map((data, index) => (
+                  <tr>
+                    <td className="text-center">{index + 1}</td>
+                    <td>
+                      <img
+                        src={data.image}
+                        height={250}
+                        width={250}
+                        onClick={() => {
+                          setModal({ image: data.image, bool: true });
+                        }}
+                      ></img>
+                    </td>
+                    <td>{data.propmt}</td>
+                    <td>{data.negativePrompt}</td>
+                    <td className="text-right">
+                      <Button className="btn-icon" color="info" size="sm">
+                        <i className="tim-icons icon-upload"></i>
                       </Button>
-                      <UncontrolledTooltip
-                        delay={0}
-                        placement='right'
-                        target='tooltip341148792'
-                      >
-                        Can't wait for your message
-                      </UncontrolledTooltip>
-                    </Form>
-                  </CardBody>
-                </Card>
-              </Col>
-              <Col className='ml-auto' md='4'>
-                <div className='info info-horizontal'>
-                  <div className='icon icon-primary'>
-                    <i className='tim-icons icon-square-pin' />
-                  </div>
-                  <div className='description'>
-                    <h4 className='info-title'>Find us at the office</h4>
-                    <p>
-                      Bld Mihail Kogalniceanu, nr. 8, <br />
-                      7652 Bucharest, <br />
-                      Romania
-                    </p>
-                  </div>
-                </div>
-                <div className='info info-horizontal'>
-                  <div className='icon icon-primary'>
-                    <i className='tim-icons icon-mobile' />
-                  </div>
-                  <div className='description'>
-                    <h4 className='info-title'>Give us a ring</h4>
-                    <p>
-                      Michael Jordan <br />
-                      +40 762 321 762 <br />
-                      Mon - Fri, 8:00-22:00
-                    </p>
-                  </div>
-                </div>
-              </Col>
-            </Row>
+                      <Button className="btn-icon" color="danger" size="sm">
+                        <i className="tim-icons icon-trash-simple"></i>
+                      </Button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
           </Container>
-        </section>
+        </div>
         <Footer />
       </div>
     </>
-  )
+  );
 }
